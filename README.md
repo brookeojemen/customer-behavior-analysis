a0f7d245433c414eb2b2ac00cc935f875010a80f
Bao004 <brooke.ojemen@gmail.com>
2026-07-22 16:28:39 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -5,6 +5,8 @@
 This project is an analysis of a dataset from the UC Irvine Machine Learning Repository that tracked transactions for a United Kingdom based
 online retail. Using python, the goal is to figure out the probability of a returning customer, and what factors contribute to it. 
 
+This project analyzes customer purchasing behavior in an online retail dataset to identify repeat customers, understand their purchasing patterns, and determine factors associated with customer retention.
+
 What factors influence customer retention and repeat purchasing behavior in an online retail dataset?
 
 WHich items are being bought the most? 
--- main.py
+++ main.py
@@ -2,10 +2,13 @@ import pandas as pd
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as plot
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import classification_report
 
 ###df = pd.read_csv("data/Online Retail.csv")
 df = pd.read_csv("retail_data.csv", encoding="latin1")
-df = df.drop(columns=['InvoiceNo', 'Country', 'Description'])
+df = df.drop(columns=['Country', 'Description'])
 
 fault_StockCode = ["POST", "DOT", "D"]
 
@@ -21,10 +24,36 @@ df.info()
 print(df.describe())
 print(df.head(10))
 
-### make a bar graph to see the most bought items, then ill make another graph too see of those customers 
-### bought it if they are buying it more than once
-### thought process, add up quantityes for each stockcode using a forloop maybe
+print(item_by_stockCode.describe()) 
+### the most populat item is 85123A(white 
+#hanging heart t-light holder) and was in 2035 orders,there are a total of 
+#3663 items being accounted for it the data set
 
-print(item_by_stockCode.describe())
+popular_item = df[item_by_stockCode =="85123A"]
+print(popular_item["CustomerID"].value_counts())
+print(popular_item.head(50))
+###856 total groups order this product
+
+###keep track of how many unique invoice numbers a person has, that means they are a returning customer
+
+print(df["InvoiceNo"].value_counts()) ### biggest order made was 541, invoice number 576339, person 14096, in december
+biggest_order_person = (df[df["CustomerID"] == 14096])
+print(biggest_order_person)
+print(biggest_order_person["InvoiceNo"].value_counts()) ### this person biggest orders happen around december christmas related items
+
+
+
+
+
+'''
+X_train, X_test, Y_train, Y_trust = train_test_split(
+
+
+)
+'''
+
+
+
+
 
 
