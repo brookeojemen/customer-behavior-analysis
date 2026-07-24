8183e3450eca1b5b0f3dbb8c5e43e6f855ccd7b1
Bao004 <brooke.ojemen@gmail.com>
2026-07-24 13:14:50 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -17,29 +17,76 @@ df = df[df["Quantity"] > 0]
 df = df[df["UnitPrice"] > 0]
 df = df[~df["StockCode"].isin(fault_StockCode)]
 
-quantity = df["Quantity"]
-item_by_stockCode = df["StockCode"]
+
+StockCode = df["StockCode"]
+InvoiceNo = df["InvoiceNo"]
 
 df.info()
 print(df.describe())
 print(df.head(10))
 
-print(item_by_stockCode.describe()) 
-### the most populat item is 85123A(white 
+print(StockCode.describe()) 
+print(df["StockCode"].value_counts())
+### the most popular item is 85123A(white 
 #hanging heart t-light holder) and was in 2035 orders,there are a total of 
 #3663 items being accounted for it the data set
 
-popular_item = df[item_by_stockCode =="85123A"]
+print(InvoiceNo.describe())
+print(df["InvoiceNo"].value_counts())
+### biggest order made was 541, invoice number 576339, 
+# in december
+# CustomerID 14096
+
+
+popular_item = df[StockCode =="85123A"]
 print(popular_item["CustomerID"].value_counts())
 print(popular_item.head(50))
 ###856 total groups order this product
 
-###keep track of how many unique invoice numbers a person has, that means they are a returning customer
 
-print(df["InvoiceNo"].value_counts()) ### biggest order made was 541, invoice number 576339, person 14096, in december
 biggest_order_person = (df[df["CustomerID"] == 14096])
 print(biggest_order_person)
-print(biggest_order_person["InvoiceNo"].value_counts()) ### this person biggest orders happen around december christmas related items
+print(biggest_order_person["InvoiceNo"].value_counts()) 
+print(biggest_order_person["StockCode"].value_counts()) 
+### this person biggest orders happen around december christmas related items
+# buys stock item 23263 item the most
+
+
+unique_InvoiceNo = df["InvoiceNo"].groupby(df["CustomerID"]).nunique()
+print(unique_InvoiceNo.describe())
+print(unique_InvoiceNo.info)
+print(unique_InvoiceNo.idxmax())
+print(unique_InvoiceNo[unique_InvoiceNo >= 50])
+print(unique_InvoiceNo.loc[14096])
+### most returned customer returned 209 times
+# 4338 total unique customers 
+# CustomerID 14096 made 17 purchases, but makes big orders
+
+ID12748 = df[df["CustomerID"] == 12748]
+ID14911 = df[df["CustomerID"] == 14911]
+ID17841 = df[df["CustomerID"] == 17841]
+
+print(ID12748.head(10))
+print(ID14911.head(10))
+print(ID17841.head(10))
+### Top 3 returners, all over 100
+
+
+
+
+'''
+
+
+
+
+
+
+
+'''
+
+
+
+
 
 
 
